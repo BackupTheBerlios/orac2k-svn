52b11930d1db0b622aa2fb42bc4d9eb4cbf07718
mmarchi <mmarchi@861e406f-924c-0410-b2bf-b800038c30ee>
2005-12-08 16:26:19 +0000

    *** empty log message ***

--- trunk/orac2k/source/Module_Neighbors.f90
+++ trunk/orac2k/source/Module_Neighbors.f90
@@ -1,5 +1,5 @@
 !!$***********************************************************************
-!!$   Time-stamp: <2005-12-08 14:41:45 tarmo>                           *
+!!$   Time-stamp: <2005-12-08 17:24:47 marchi>                           *
 !!$                                                                      *
 !!$                                                                      *
 !!$                                                                      *
@@ -14,56 +14,46 @@
 
 !!$---- This subroutine is part of the program ORAC ----*
 MODULE Module_Neighbors
-IMPLICIT none
-TYPE Neighbors
-INTEGER :: no
-INTEGER, DIMENSION (:), POINTER :: nb
-END TYPE Neighbors
-TYPE(Neighbors), DIMENSION (:), POINTER, SAVE :: neigha
-TYPE(Neighbors), DIMENSION (:), POINTER, SAVE :: neighb
-TYPE(Neighbors), DIMENSION (:), POINTER, SAVE :: neighc
+  IMPLICIT none
+  TYPE Neighbors
+     INTEGER :: no
+     INTEGER, DIMENSION (:), POINTER :: nb
+  END TYPE Neighbors
+  TYPE(Neighbors), DIMENSION (:), POINTER, SAVE :: neigha
+  TYPE(Neighbors), DIMENSION (:), POINTER, SAVE :: neighb
+  TYPE(Neighbors), DIMENSION (:), POINTER, SAVE :: neighc
 CONTAINS
-SUBROUTINE Start(neigh,nmol)
-IMPLICIT none
-TYPE(Neighbors), DIMENSION (:), POINTER :: neigh
-INTEGER :: nmol
+  SUBROUTINE Start(neigh,nmol)
+    IMPLICIT none
+    TYPE(Neighbors), DIMENSION (:), POINTER :: neigh
+    INTEGER :: nmol
+    
+    INTEGER :: i
+    ALLOCATE(neigh(nmol))
+    DO i=1,nmol
+       neigh(i) % no = 0
+       NULLIFY(neigh(i) % nb)
+    END DO
+  END SUBROUTINE Start
+  SUBROUTINE Delete(neigh)
+    IMPLICIT none
+    INTEGER :: i,ierr
+    TYPE(Neighbors), DIMENSION (:), POINTER :: neigh
+    
+    IF(ASSOCIATED(neigh)) THEN
+       DO i=1,SIZE(neigh)
+          IF(ASSOCIATED(neigh(i) % nb)) THEN
+             DEALLOCATE(neigh(i) % nb, STAT=ierr)
+             IF(ierr /= 0) THEN
+                WRITE(*,*) 'ierr not zero',ierr
+             END IF
+             NULLIFY(neigh(i) % nb)
+          END IF
+       END DO
 
-INTEGER :: i
-ALLOCATE(neigh(nmol))
-DO i=1,nmol
-neigh(i) % no = 0
-NULLIFY(neigh(i) % nb)
-END DO
-END SUBROUTINE Start
-SUBROUTINE Delete(neigh)
-IMPLICIT none
-INTEGER :: i,ierr
-TYPE(Neighbors), DIMENSION (:), POINTER :: neigh
-
-IF(ASSOCIATED(neigh)) THEN
-DO i=1,SIZE(neigh)
-  IF(ASSOCIATED(neigh(i) % nb)) THEN
-     DEALLOCATE(neigh(i) % nb, STAT=ierr)
-     IF(ierr /= 0) THEN
-	WRITE(*,*) 'ierr not zero',ierr
-     END IF
-     NULLIFY(neigh(i) % nb)
-  END IF
-END DO
-
-DEALLOCATE(neigh, STAT=ierr)
-NULLIFY(neigh)
-END IF
-!!$    IF(ASSOCIATED(neigh)) THEN
-!!$       IF(SIZE(neigh) > 0) THEN
-!!$          DO i=1,SIZE(neigh)
-!!$             IF(ASSOCIATED(neigh(i) % nb)) THEN
-!!$                DEALLOCATE(neigh(i) % nb)
-!!$             END IF
-!!$          END DO
-!!$          DEALLOCATE(neigh)
-!!$       END IF
-!!$    END IF
-END SUBROUTINE Delete
+       DEALLOCATE(neigh, STAT=ierr)
+       NULLIFY(neigh)
+    END IF
+  END SUBROUTINE Delete
 END MODULE Module_Neighbors
 
