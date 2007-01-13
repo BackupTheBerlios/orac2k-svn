204b633fe9c3a651af0399a9dd739a15ca59e49c
mmarchi <mmarchi@861e406f-924c-0410-b2bf-b800038c30ee>
2007-01-13 00:11:54 +0000

    *** empty log message ***

--- trunk/oracDD/src/PDB.f90
+++ trunk/oracDD/src/PDB.f90
@@ -1,7 +1,7 @@
 MODULE PDB
 
 !!$***********************************************************************
-!!$   Time-stamp: <2007-01-12 20:39:41 marchi>                           *
+!!$   Time-stamp: <2007-01-13 01:09:49 marchi>                           *
 !!$                                                                      *
 !!$                                                                      *
 !!$                                                                      *
@@ -40,9 +40,12 @@ MODULE PDB
   INTEGER, SAVE, POINTER :: Res_Atm(:,:)=>NULL()
   INTEGER, SAVE, POINTER :: Grp_Atm(:,:)=>NULL()
   INTEGER, SAVE, POINTER :: SltSlv(:,:)=>NULL()
-  CHARACTER(len=max_char), PARAMETER :: water(8)=(/'wat  ','hoh  ','tip3 '&
-       &,'tip3p','tip3f','spc  ','hoh  ','spce '/)
-  CHARACTER(len=max_char), PARAMETER :: histidine(5)=(/'his','hsd','hse','hsp','hs2'/)
+  TYPE :: Name_Exception
+     SEQUENCE
+     CHARACTER(len=max_char), ALLOCATABLE :: res(:)
+     CHARACTER(len=max_char), ALLOCATABLE :: lab(:)
+  END TYPE Name_Exception
+  TYPE(Name_Exception), SAVE :: ex(4)
 CONTAINS
   FUNCTION PDB_(Type,PDB_string,ResPdb) RESULT(out)
 !!$****************************************************************************
@@ -195,13 +198,29 @@ CONTAINS
     LOGICAL :: ok
     CHARACTER(len=max_char) :: lab_p,lab_m,res_n,lab_m0,res_m
     CHARACTER(len=max_char), POINTER  :: new_name(:)
-    
+
+!!$
+!!$--- Get the beginning and end of each residue atom
+!!$    
+
     Res_Atm=>IndSequence__Res()
+
+!!$
+!!$--- Get the beginning and end of each residue for solute and
+!!$--- solvent
+!!$    
+
     SltSlv=>IndSequence__sltslv_Res()
+
     ip_res=1
     IF(MY_Fam('solvent',type)) ip_res=2
 
     out=.TRUE.
+
+!!$
+!!$--- Check if Tpg is initialized
+!!$
+
     IF(.NOT. ALLOCATED(Tpg % atm)) THEN
        errmsg_f='Cannot validate PDB coordinates if system&
             & topology is not defined'
@@ -209,6 +228,11 @@ CONTAINS
        out=.FALSE.
        RETURN
     END IF
+
+!!$
+!!$--- Check if the No. of residues correspond with those found
+!!$--- in the .PDB file
+!!$
 
     IF(SltSlv(2,ip_Res)-SltSlv(1,ip_Res)+1 /= SIZE(ResPdb)) THEN
        errmsg_f='No. of residues in '//TRIM(Type)//' .pdb file&
@@ -219,8 +243,15 @@ CONTAINS
        RETURN       
     END IF
 
+!!$
+!!$--- oks are .true. only if all Tpg atoms are found on the .pdb with
+!!$--- the exception of the hydrogens
+!!$
+
     ALLOCATE(oks(SIZE(Tpg % atm)))
     oks=.FALSE.
+
+!!$-- Start the check on the secondary sequence
 
     DO n=SltSlv(1,ip_res),SltSlv(2,ip_res)
        Begins=Res_Atm(1,n)
@@ -228,11 +259,12 @@ CONTAINS
        res_n=ADJUSTL(Tpg % atm(Begins) % a % Res)
 
        nword=MyParse_(res_n)
-       IF(TRIM(strngs(1)) == 'Link') THEN
-          res_n=ADJUSTL(strngs(2))
+       IF(TRIM(strngs(1)) == 'Link') THEN  !-- If the residue is linked
+          res_n=ADJUSTL(strngs(2))          !-- take its real name
        END IF
-       IF(.NOT. My_Fam(ResPdb(n) % ResName, Res_n)) THEN
-          IF(.NOT. ResName_Exceptions(ResPdb(n) % ResName,&
+!!$--- See if the ResNames match
+       IF(.NOT. My_Fam(ResPdb(n) % ResName, Res_n)) THEN 
+          IF(.NOT. ResName_Exceptions(ResPdb(n) % ResName,& ! There are exceptions...
                & res_n)) THEN
              errmsg_w='System Topology residue type '&
                   &//TRIM(res_n)//' does not match &
@@ -242,10 +274,6 @@ CONTAINS
              CALL Add_Errors(1,errmsg_w)
           END IF
        END IF
-
-       DO m=Begins,Ends
-          IF(Tpg % atm(m) % a % beta(1:1) ==  'h') oks(m)=.TRUE.
-       END DO
 
        DO m=1,SIZE(ResPdb(n) % atm)
           lab_m0=ResPdb(n) % atm(m) % AtmName
@@ -258,8 +286,7 @@ CONTAINS
                 lab_p=Tpg % atm (p) % a % beta
                 IF(TRIM(lab_m) == TRIM(Lab_p)) THEN
                    ok=.TRUE.
-                   oks(p)=.TRUE.
-                   
+                   oks(p)=.TRUE.                   
                 END IF
              END DO
           END DO
@@ -272,9 +299,13 @@ CONTAINS
              out=.FALSE.
           END IF
        END DO
+       ok=.TRUE.
+       DO m=Begins,Ends
+          IF(Tpg % atm(m) % a % beta(1:1) ==  'h' .AND. oks(m)) ok=.FALSE.
+       END DO
        DO m=Begins,Ends
           lab_m=Tpg % atm (m) % a % beta
-          IF(.NOT. oks(m)) THEN
+          IF((.NOT. oks(m)) .AND. (Tpg % atm(m) % a % beta(1:1) /=  'h' )) THEN
              errmsg_f='While reading .PDB coordinates for residue No. '&
                   &//TRIM(Myputnum(n))//', cannot found label '//TRIM(lab_m)&
                   &//' of residue '//TRIM(res_n)
@@ -289,47 +320,74 @@ CONTAINS
     SUBROUTINE AtomName_Exceptions(lab, res, new_lab)
       CHARACTER(len=*) :: lab,res
       CHARACTER(len=max_char), POINTER :: new_lab(:)
+      INTEGER :: n,m,o,p,len0
+      INTEGER, SAVE :: first_Time=0
+      IF(First_Time == 0) THEN
+         First_Time=1
+         ALLOCATE(ex(1) % res(1)); ALLOCATE(ex(1) % lab(2))
+         ALLOCATE(ex(2) % res(4)); ALLOCATE(ex(2) % lab(3))
+         ALLOCATE(ex(3) % res(1)); ALLOCATE(ex(3) % lab(2))
+         ALLOCATE(ex(4) % res(1)); ALLOCATE(ex(4) % lab(2))
+         ex=(/Name_Exception((/'ile'/),(/'cd ','cd1'/))&
+              &, Name_Exception((/'hoh','tip','wat','spc'/),(/'o','o1','oh2'/))&
+              &, Name_Exception((/' '/),(/'oct1','ot1'/))&
+              &, Name_Exception((/' '/),(/'oct2','ot2'/)) /)
+      END IF
 
       IF(ASSOCIATED(new_lab)) DEALLOCATE(new_lab)
-      IF(TRIM(res) == 'ile') THEN
-         IF(TRIM(lab) == 'cd' .OR. TRIM(lab) == 'cd1'  ) THEN
-            ALLOCATE(new_lab(2))
-            new_lab(1)='cd'
-            new_lab(2)='cd1'
-            RETURN
-         END IF
-      END IF
-      IF(TRIM(res) == 'hoh' .OR. TRIM(res) == 'tip'&
-           & .OR. TRIM(res) == 'wat' .OR. TRIM(res) == 'spc') THEN
-         IF(TRIM(lab) == 'o' .OR. TRIM(lab) == 'o1' .OR. TRIM(lab) == 'oh2'  ) THEN
-            ALLOCATE(new_lab(3))
-            new_lab(1)='o'
-            new_lab(2)='o1'
-            new_lab(2)='oh2'
-            RETURN
-         END IF
-      END IF
-      IF(TRIM(lab) == 'oct1' .OR. TRIM(lab) == 'ot1') THEN
-         ALLOCATE(new_lab(2))
-         new_lab(1)='oct1'
-         new_lab(2)='ot1'
-         RETURN
-      END IF
+      DO n=1,SIZE(ex)
+
+!!$
+!!$--- Check if the right ewsidue
+!!$
+         o=0
+         DO m=1,SIZE(ex (n) % res)
+            IF(TRIM(ex(n) % res(m)) == TRIM(res)) o=o+1
+         END DO
+
+!!$
+!!$--- For a blank residue do always a check on the atomic labels
+!!$
          
-      IF(TRIM(lab) == 'oct2' .OR. TRIM(lab) == 'ot2') THEN
-         ALLOCATE(new_lab(2))
-         new_lab(1)='oct2'
-         new_lab(2)='ot2'
-         RETURN
-      END IF
+         IF(SIZE(ex(n) % res) == 1) THEN
+            len0=LEN_TRIM(ex(n) % res(1))
+            IF(len0 == 0) o=1
+         END IF       
+!!$
+!!$--- Finally check on the atomic labels
+!!$
+         IF(o /= 0) THEN
+            p=0
+            DO m=1,SIZE(ex(n) % lab)
+               IF(TRIM(ex(n) % lab(m)) == TRIM(lab)) p=p+1
+            END DO
+            IF(p == 0) CYCLE
+            
+            m=SIZE(ex(n) % lab)
+            ALLOCATE(new_lab(m))
+            new_lab=ADJUSTL(ex(n) % lab)
+            RETURN
+         END IF
+      END DO
+!!$
+!!$--- All tests have failed use the original label only
+!!$
       ALLOCATE(new_lab(1)); new_lab(1)=lab
     END SUBROUTINE AtomName_Exceptions
     FUNCTION ResName_Exceptions(lab_r, lab_t) RESULT(out)
       CHARACTER(len=*) :: lab_r,lab_t
       LOGICAL :: out
       INTEGER :: i,c_r,c_t
+      CHARACTER(len=max_char), PARAMETER :: water(8)=(/'wat  ','hoh  ','tip3 '&
+           &,'tip3p','tip3f','spc  ','hoh  ','spce '/)
+      CHARACTER(len=max_char), PARAMETER :: histidine(5)=(/'his','hsd'&
+           &,'hse','hsp','hs2'/)
 
       out=.FALSE.
+!!$
+!!$--- Try to see if the residue name corresponds to a common
+!!$--- name of water
+!!$
       c_r=0
       c_t=0
       DO i=1,SIZE(water)
@@ -340,6 +398,10 @@ CONTAINS
          out=.TRUE.
          RETURN
       END IF
+!!$
+!!$--- Try to see if the residue name corresponds to a common 
+!!$--- name of histidine
+!!$
       c_r=0
       c_t=0
       DO i=1,SIZE(histidine)
