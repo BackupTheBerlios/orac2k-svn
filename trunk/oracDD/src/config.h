f5c054bf7036d9cd561a571df91d49603a64abdc
mmarchi <mmarchi@861e406f-924c-0410-b2bf-b800038c30ee>
2009-02-14 10:44:23 +0000

    Modified config.h and replaced with parameters.h

--- trunk/oracDD/src/config.h
+++ trunk/oracDD/src/config.h
@@ -4,40 +4,17 @@
  * $Log$
  */
 
-#define _SIMPLE_ 0
-#define _PDB_    1
-#define _ORAC_   2
-#define _OTHER_  3
+#ifndef _CONFIG_H
+#define _CONFIG_H
 
-#define _INIT_ 0
-#define _FOLD_ 1
-#define _N0_     1
-#define _N1_     2
-#define _M_      3
-#define _L_      4
-#define _H_      5
+/*
+  Define type of pointt-point communications: 
+        __Blocking       using mpi_sendrecv  
+	__NonBlocking    using mpi_isend and mpi_irecv
+	
+#define __Blocking
+*/
 
-#define _CONSTRAINTS_ 1
+#define __NonBlocking
 
-#define _INIT_EXCHANGE_     0
-#define _EXCHANGE_ONLY_     1
-#define _EXCHANGE_          1
-#define _CONSTRS_           0
-#define _STRETCHS_          1
-#define _ANGLES_            2
-#define _IMPHS_             3
-#define _TORSIONS_          4
-#define _INT14S_            5
-
-#define _PME_ 1
-
-#define _X_ 1
-#define _Y_ 2
-#define _Z_ 3
-#define  _PLUS_  1
-#define _MINUS_ -1
-#define _2NDHALF_ 1
-
-#define _XA_TO_X_   1
-#define _X_TO_XA_  -1
-#define _COUNT_ONCE_ 1
+#endif
